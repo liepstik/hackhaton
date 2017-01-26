c980c93dc109fe769ddd47a84f3a7b7056ca590e
Laura <laura.liechitz@gmail.com>
2017-01-26 22:52:15 +0100

    initial commit: templated basic architecture

---
+++ controller/game1.ctrl.php
@@ -0,0 +1,5 @@
+<?php
+  include("../view/header.view.html");
+  include("../view/game1.view.html");
+  include("../view/footer.view.html");
+?>
---
+++ controller/start.ctrl.php
@@ -0,0 +1,5 @@
+<?php
+  include("../view/header.view.html");
+  include("../view/start.view.html");
+  include("../view/footer.view.html");
+?>
