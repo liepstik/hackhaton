da56dcc702a6c2c6e041a9d72e656a6c1bd44b2d
el.achraf <el.achraf150@gmail.com>
2017-01-28 14:09:01 +0100

    new: js display & hide div

--- view/js/init.js
+++ view/js/init.js
@@ -6,3 +6,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function displayNone() {
+	document.getElementById("none-div").style.display = "block";
+    document.getElementById("block-div").style.display = "none";
+    document.getElementById("download-button").onclick = displayBlock();
+}
+
+function displayBlock() {
+	document.getElementById("none-div").style.display = "none";
+    document.getElementById("block-div").style.display = "block";
+    document.getElementById("download-button").onclick = displayNone();
+}
