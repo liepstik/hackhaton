792ea5fad34636957b09385d4da4503071ae6fa3
Laura <laura.liechitz@gmail.com>
2017-01-28 17:41:18 +0100

    fix: div hide & show

--- view/js/init.js
+++ view/js/init.js
@@ -7,14 +7,23 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function displayNone() {
-	document.getElementById("none-div").style.display = "block";
-    document.getElementById("block-div").style.display = "none";
-    document.getElementById("download-button").onclick = displayBlock();
-}
+//function displayNone() {
+//	document.getElementById("none-div").style.display = "block";
+//    document.getElementById("block-div").style.display = "none";
+//    document.getElementById("download-button").onclick = displayBlock();
+//}
+//
+//function displayBlock() {
+//	document.getElementById("none-div").style.display = "none";
+//    document.getElementById("block-div").style.display = "block";
+//    document.getElementById("download-button").onclick = displayNone();
+//}
 
-function displayBlock() {
-	document.getElementById("none-div").style.display = "none";
-    document.getElementById("block-div").style.display = "block";
-    document.getElementById("download-button").onclick = displayNone();
-}
+$(document).ready(function(){
+    $("#download-button").click(function(){
+        $("#index-banner").hide();
+        $("#div-b").show();
+        
+    });
+    
+});
