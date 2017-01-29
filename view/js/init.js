1fc6331e5b9333825acd90c33826d07c976c8b76
Laura <laura.liechitz@gmail.com>
2017-01-29 06:50:17 +0100

    fix: page d'accueil

--- view/js/init.js
+++ view/js/init.js
@@ -19,11 +19,38 @@
 //    document.getElementById("download-button").onclick = displayNone();
 //}
 
-$(document).ready(function(){
+$(document).ready(function() {
+    console.log(document.URL);
+    $.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback', function(data) {
+       console.log("the json " + data); 
+    //data is the JSON string
+});
+    $.getJSON(document.URL, function(data) {
+       console.log("my json " + data); 
+    });
     $("#download-button").click(function(){
         $("#index-banner").hide();
-        $("#div-b").show();
-        
+        $("#div-b").show();     
     });
     
+    
 });
+
+function sendElement() { 
+    var firstName=document.getElementById("first_name").value;
+    var currentAge=document.getElementById("current_age").value;
+    var retirementAge=document.getElementById("retirement_age").value;
+    
+    if (firstName == "" || currentAge == "" || retirementAge == "")
+        console.log("INFO PLS");
+    else
+    {
+        var myObj = {"firstname": firstName,
+                     "actualage": currentAge,
+                     "retireage": retirementAge };
+        console.log(myObj);
+        var myJSON = JSON.stringify(myObj);
+        console.log(myJSON);
+        window.location = "../controller/start.ctrl.php?x=" + myObj;    
+    }
+}
